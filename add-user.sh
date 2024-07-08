8713a9356411183697289d1de9e912b7ef921e76
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-09 00:16:24 +0700

    Update add-user.sh

--- add-user.sh
+++ add-user.sh
@@ -5,7 +5,6 @@ sudo adduser --disabled-password --gecos "" $SSH_USERNAME --force-badname
 echo "$SSH_USERNAME:$SSH_PASSWORD" | sudo chpasswd
 sudo usermod -aG sudo $SSH_USERNAME
 
-
 clear
 echo "SSH login information:"
 echo "Username: $SSH_USERNAME"
