a708b85a2f2f346d1f8797f3f8d08ddaaf50864e
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-09 00:12:31 +0700

    Create add-user.sh

---
+++ add-user.sh
@@ -0,0 +1,14 @@
+SSH_USERNAME=$2
+SSH_PASSWORD=$3
+
+sudo adduser --disabled-password --gecos "" $SSH_USERNAME --force-badname
+echo "$SSH_USERNAME:$SSH_PASSWORD" | sudo chpasswd
+sudo usermod -aG sudo $SSH_USERNAME
+
+
+clear
+echo "SSH login information:"
+echo "Username: $SSH_USERNAME"
+echo "Password: $SSH_PASSWORD"
+echo "Hostname: $$(hostname -I | cut -d' ' -f1)"
+echo "Use the above information to connect using PuTTY or any SSH client."
