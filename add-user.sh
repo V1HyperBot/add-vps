24e91f565aef29bd84be4a3fbb097b5575ba2684
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-09 11:53:21 +0700

    Update add-user.sh

--- add-user.sh
+++ add-user.sh
@@ -6,49 +6,37 @@ BOT_TOKEN="7419614345:AAFwmSvM0zWNaLQhDLidtZ-B9Tzp-aVWICA"
 
 send_telegram_message() {
     local MESSAGE=$1
-    local REPLY_MARKUP='{
-        "inline_keyboard": [
-            [
-                {
-                    "text": "Powered By",
-                    "url": "https://t.me/NorSodikin"
-                }
-            ]
-        ]
-    }'
-    
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-    -d chat_id=$CHAT_ID \
-    -d text="$MESSAGE" \
-    -d parse_mode="Markdown" \
-    -d reply_markup="$REPLY_MARKUP"
-    clear 
+    local INLINE_KEYBOARD='{"inline_keyboard":[[{"text":"powered by","url":"https://t.me/NorSodikin"}]]}'
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE" -d parse_mode=Markdown -d reply_markup="$INLINE_KEYBOARD"
+    clear
 }
-
 
 case $ACTION in
   add)
-    sudo adduser --disabled-password --gecos "" $SSH_USERNAME --force-badname
-    echo "$SSH_USERNAME:$SSH_PASSWORD" | sudo chpasswd
-    sudo usermod -aG sudo $SSH_USERNAME
+    if id "$SSH_USERNAME" &>/dev/null; then
+        MESSAGE="User $SSH_USERNAME already exists. Please choose a different username."
+    else
+        sudo adduser --disabled-password --gecos "" $SSH_USERNAME --force-badname
+        echo "$SSH_USERNAME:$SSH_PASSWORD" | sudo chpasswd
+        sudo usermod -aG sudo $SSH_USERNAME
 
-    HOSTNAME=$(hostname -I | cut -d' ' -f1)
-    MESSAGE="SSH login information:%0A%0AUsername: $SSH_USERNAME%0APassword: $SSH_PASSWORD%0AHostname: $HOSTNAME%0A%0AUse the above information to connect using PuTTY or any SSH client."
+        HOSTNAME=$(hostname -I | cut -d' ' -f1)
+        MESSAGE="*SSH login information:*%0A%0A*Username:* $SSH_USERNAME%0A*Password:* $SSH_PASSWORD%0A*Hostname:* $HOSTNAME%0A%0A_Use the above information to connect using PuTTY or any SSH client._"
+    fi
 
     send_telegram_message "$MESSAGE"
-
-    echo -e "SSH login information:\n\nUsername: $SSH_USERNAME\nPassword: $SSH_PASSWORD\nHostname: $HOSTNAME\n\nUse the above information to connect using PuTTY or any SSH client."
     ;;
 
   delete)
-    sudo usermod --expiredate 1 $SSH_USERNAME
-    sudo deluser --remove-home $SSH_USERNAME
+    if ! id "$SSH_USERNAME" &>/dev/null; then
+        MESSAGE="User $SSH_USERNAME does not exist."
+    else
+        sudo usermod --expiredate 1 $SSH_USERNAME
+        sudo deluser --remove-home $SSH_USERNAME
+        MESSAGE="User $SSH_USERNAME has been deleted from the system and can no longer log in."
+    fi
 
-    MESSAGE="User $SSH_USERNAME has been deleted from the system and can no longer log in."
-    
     send_telegram_message "$MESSAGE"
-
-    echo -e "User $SSH_USERNAME has been deleted from the system and can no longer log in."
     ;;
 
   *)
