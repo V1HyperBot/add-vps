d96a0fd1d55be2fe2a49b3110b11550f5262a2f5
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-09 10:51:23 +0700

    Update add-user.sh

--- add-user.sh
+++ add-user.sh
@@ -1,13 +1,41 @@
+ACTION=$1
 SSH_USERNAME=$2
 SSH_PASSWORD=$3
+CHAT_ID=$4
+BOT_TOKEN="7419614345:AAFwmSvM0zWNaLQhDLidtZ-B9Tzp-aVWICA"
 
-sudo adduser --disabled-password --gecos "" $SSH_USERNAME --force-badname
-echo "$SSH_USERNAME:$SSH_PASSWORD" | sudo chpasswd
-sudo usermod -aG sudo $SSH_USERNAME
+send_telegram_message() {
+    local MESSAGE=$1
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE"
+    clear
+}
 
-clear
-echo "SSH login information:"
-echo "Username: $SSH_USERNAME"
-echo "Password: $SSH_PASSWORD"
-echo "Hostname: $(hostname -I | cut -d' ' -f1)"
-echo "Use the above information to connect using PuTTY or any SSH client."
+case $ACTION in
+  add)
+    sudo adduser --disabled-password --gecos "" $SSH_USERNAME --force-badname
+    echo "$SSH_USERNAME:$SSH_PASSWORD" | sudo chpasswd
+    sudo usermod -aG sudo $SSH_USERNAME
+
+    HOSTNAME=$(hostname -I | cut -d' ' -f1)
+    MESSAGE="SSH login information:%0A%0AUsername: $SSH_USERNAME%0APassword: $SSH_PASSWORD%0AHostname: $HOSTNAME%0A%0AUse the above information to connect using PuTTY or any SSH client."
+
+    send_telegram_message "$MESSAGE"
+
+    echo -e "SSH login information:\n\nUsername: $SSH_USERNAME\nPassword: $SSH_PASSWORD\nHostname: $HOSTNAME\n\nUse the above information to connect using PuTTY or any SSH client."
+    ;;
+
+  delete)
+    sudo usermod --expiredate 1 $SSH_USERNAME
+    sudo deluser --remove-home $SSH_USERNAME
+
+    MESSAGE="User $SSH_USERNAME has been deleted from the system and can no longer log in."
+    
+    send_telegram_message "$MESSAGE"
+
+    echo -e "User $SSH_USERNAME has been deleted from the system and can no longer log in."
+    ;;
+
+  *)
+    echo "Invalid action. Use 'add' to add a user or 'delete' to delete a user."
+    ;;
+esac
