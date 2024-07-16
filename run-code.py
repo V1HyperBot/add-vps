e4b4efa8420f9bdf3e8753061c6c923b09ae7d19
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-17 01:24:00 +0700

    Delete run-code.py

--- run-code.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-command = input("Silakan masukkan kode encrypted anda: ")
-
-try:
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    print(result.stdout)
-except subprocess.CalledProcessError as e:
-    print("Error:", e.stderr)
