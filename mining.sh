507f9b51d2d167400ff718fc2e8c7cf154d9f1df
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-18 21:12:56 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,11 +1,12 @@
 coin=$1
 wallet=$2
-workers=$3
 
-if [ -z "$coin" ] || [ -z "$wallet" ] || [ -z "$workers" ]; then
-  echo "🚫 Penggunaan: bash mining.sh coin wallet workers 🚫"
+if [ -z "$coin" ] || [ -z "$wallet" ]; then
+  echo "🚫 Penggunaan: bash mining.sh coin wallet 🚫"
   exit 1
 fi
+
+workers=$(tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
 
 echo "🔽 Download XMRig miner 🔽"
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz
@@ -21,8 +22,8 @@ echo "#              <⛏️ XMRIG MINER ⛏️>              #"
 echo "#           🔄 ALGORITME RANDOM(X) 🔄           #"
 echo "#################################################"
 
-printf "| %-10s | %-$(expr length "$wallet")s | %-2s |\n" "COIN" "WALLET" "WORKERS"
-printf "| %-10s | %-$(expr length "$wallet")s | %-2s |\n" "$coin" "$wallet" "$workers"
+printf "| %-10s | %-$(expr length "$wallet")s | %-8s |\n" "COIN" "WALLET" "WORKERS"
+printf "| %-10s | %-$(expr length "$wallet")s | %-8s |\n" "$coin" "$wallet" "$workers"
 sleep 5
 
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u $coin:$wallet.$workers -p x
