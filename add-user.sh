07a384b2a053870e374229ce16269cedf8a1f0a2
𝗛𝗬𝗣𝗘𝗥 𝗥𝗢𝗕𝗢𝗧 <173705945+V1HyperBot@users.noreply.github.com>
2024-07-23 00:59:46 +0700

    Update add-user.sh

--- add-user.sh
+++ add-user.sh
@@ -1,11 +1,14 @@
-
-send_telegram_message() {
+function send_telegram_message() {
     local MESSAGE=$1
     local INLINE_KEYBOARD='{"inline_keyboard":[[{"text":"Powered By","url":"https://t.me/NorSodikin"}]]}'
-    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE" -d parse_mode=Markdown -d reply_markup="$INLINE_KEYBOARD"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+        -d chat_id=$CHAT_ID \
+        -d text="$MESSAGE" \
+        -d parse_mode=Markdown \
+        -d reply_markup="$INLINE_KEYBOARD"
 }
 
-generate_random_string() {
+function generate_random_string() {
     local LENGTH=$1
     < /dev/urandom tr -dc A-Za-z0-9 | head -c $LENGTH
 }
